8adeb04fbae3aa44b1e013b7c169c660b5e64ec6
kingnsk <kingnsk2020@gmail.com>
2022-09-10 23:01:21 +0700

    learning about SOAP, ASMX etc...

---
+++ SOAP_ASMX/LibraryService/LibraryService.WebClient/Controllers/LibraryController.cs
@@ -0,0 +1,65 @@
+﻿using LibraryService.WebClient.Models;
+using LibraryServiceReference;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace LibraryService.WebClient.Controllers
+{
+    public class LibraryController : Controller
+    {
+        private readonly ILogger<LibraryController> _logger;
+
+        public LibraryController(ILogger<LibraryController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(SearchType searchType, string searchString)
+        {
+            LibraryWebServiceSoapClient client = new LibraryWebServiceSoapClient(LibraryWebServiceSoapClient.EndpointConfiguration.LibraryWebServiceSoap);
+
+            try
+            {
+                if (!string.IsNullOrEmpty(searchString) && searchString.Length >= 3)
+                {
+                    switch (searchType)
+                    {
+                        case SearchType.Title:
+
+                            return View(new BookCategoryViewModel
+                            {
+                                Books = client.GetBookByTitle(searchString)
+                            });
+                        case SearchType.Category:
+                            return View(new BookCategoryViewModel
+                            {
+                                Books = client.GetBookByCategory(searchString)
+                            });
+                        case SearchType.Author:
+                            return View(new BookCategoryViewModel
+                            {
+                                Books = client.GetBookByAuthor(searchString)
+                            });
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "Error");
+
+            }
+            return View(new BookCategoryViewModel { Books = new Book[] { }  });
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ SOAP_ASMX/LibraryService/LibraryService/Services/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryService.Services
+{
+    public interface IRepository<T, TId>
+    {
+        TId Add(T item);
+
+        int Update(T item);
+
+        int Delete(T item);
+
+        IList<T> GetAll();
+
+        T GetById(TId id);
+    }
+}
