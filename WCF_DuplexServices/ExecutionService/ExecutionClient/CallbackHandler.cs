a7466a1465c6a8d407538cf1b49fd9abfc4cb403
kingnsk <kingnsk2020@gmail.com>
2022-09-16 23:58:14 +0700

    Release WCF Execution Service and Client

---
+++ WCF_DuplexServices/ExecutionService/ExecutionClient/CallbackHandler.cs
@@ -0,0 +1,21 @@
+﻿using ExecutionClient.ExecutionServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExecutionClient
+{
+    internal class CallbackHandler : IExecutionServiceCallback
+    {
+        public void UpdateStatistics(StatisticsService statistics)
+        {
+            Console.Clear();
+            Console.WriteLine("Обновление по статистике выполнения скрипта:");
+            Console.WriteLine($"Всего         итераций: {statistics.AllCounts}");
+            Console.WriteLine($"Успешных      итераций: {statistics.SuccessCount}");
+            Console.WriteLine($"Ошибочных     итераций: {statistics.ErrorCount}");
+        }
+    }
+}
---
+++ WCF_DuplexServices/ExecutionService/ExecutionService/ExecutionService.svc.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace ExecutionService
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени класса "Service1" в коде, SVC-файле и файле конфигурации.
+    // ПРИМЕЧАНИЕ. Чтобы запустить клиент проверки WCF для тестирования службы, выберите элементы Service1.svc или Service1.svc.cs в обозревателе решений и начните отладку.
+
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerSession)]
+    public class ExecutionService : IExecutionService
+    {
+        private readonly IScriptService _scriptService;
+        private readonly IStatisticsService _statisticsService;
+        private readonly ISettingsService _settingsService;
+
+        public ExecutionService()
+        {
+            _statisticsService = new StatisticsService();
+            _settingsService = new SettingsService();
+            _scriptService = new ScriptService(_statisticsService, _settingsService, Callback);
+        }
+
+        public void RunScript()
+        {
+            _scriptService.Run(10);
+        }
+
+        public void UpdateAndCompileScript(string fileName)
+        {
+            _settingsService.FileName = fileName;
+            _scriptService.Compile();
+        }
+
+        IExecuteServiceCallback Callback
+        {
+            get
+            {
+                if (OperationContext.Current != null)
+                    return OperationContext.Current.GetCallbackChannel<IExecuteServiceCallback>();
+                else
+                    return null;
+            }
+        }
+    }
+}
