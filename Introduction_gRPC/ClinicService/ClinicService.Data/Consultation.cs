a4a429c2b8f37942e5b83ec74a55a5c302ad38e9
kingnsk <kingnsk2020@gmail.com>
2022-09-22 00:21:05 +0700

    Introductions in gRPC

---
+++ Introduction_gRPC/ClinicService/ClinicService.Data/Consultation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+//using Castle.Components.DictionaryAdapter;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClinicService.Data
+{
+    [Table("Consultations")]
+    public class Consultation
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ConsultationID { get; set; }
+
+        [ForeignKey(nameof(Client))]
+        public int ClientID { get; set; }
+
+        [ForeignKey(nameof(Pet))]
+        public int PetId { get; set; }
+
+        [Column]
+        public DateTime ConsultationDate { get; set; }
+
+        [Column]
+        public string Description { get; set; }
+        public virtual Client Client { get; set; }
+        public virtual Pet Pet { get; set; }
+    }
+}
