f4772561f25af76a01d653b8cfa7641cc15b93e1
kingnsk <kingnsk2020@gmail.com>
2022-09-29 22:47:02 +0700

    Example Fault Tolerance (Polly)

---
+++ Fault_Tolerance/PollySamples/SampleService/Services/IRootServiceClient.cs
@@ -0,0 +1,8 @@
+﻿namespace SampleService.Services
+{
+    public interface IRootServiceClient
+    {
+        public RootServiceReference.RootServiceClient Client { get; }    
+        public Task<ICollection<RootServiceReference.WeatherForecast>> Get();
+    }
+}
