195806feeab9a08bc69f341d60f6ed874dfbed87
kingnsk <kingnsk2020@gmail.com>
2022-10-01 00:11:01 +0700

    probe using SignalR

---
+++ Example_SignalR/ChatService/ChatClient/MessageClient.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using ChatService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatClient
+{
+    internal class MessageClient
+    {
+        private HubConnection _hubConnection;
+        private int _id;
+
+        public MessageClient(int id)
+        {
+            _id = id;
+            _hubConnection = new HubConnectionBuilder()
+                .WithUrl(new Uri("http://localhost:5219/hub/messages"))
+                .WithAutomaticReconnect()
+                .Build();
+            _hubConnection.StartAsync().Wait();
+            _hubConnection.On<Message>("MessageRecieved", OnMessageRecievedAsync);
+        }
+
+        private void OnMessageRecievedAsync(Message message)
+        {
+            Console.WriteLine($"#{message.FromId} -> {message.ToId} >>> {message.Text}");
+        }
+
+        public Task SendMessageAsync(int toId, string text)
+        {
+            return _hubConnection.InvokeAsync("MEssageAll", new Message
+            {
+                FromId = _id,
+                ToId = toId,
+                Text = text
+            });
+        }
+    }
+}
